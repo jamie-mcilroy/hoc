4e62f3c670b84075785440b953bd71b71f5df258
jmcilroy <jmcilroy@gbefund.com>
2023-08-30 16:52:31 -0600

    details

--- hoc_details.py
+++ hoc_details.py
@@ -1,92 +1,80 @@
+import os
 import requests
 import pandas as pd
 from bs4 import BeautifulSoup
 from io import StringIO
 
-# Initialize an empty list to hold the logs
-logs = []
-
-# Function to fetch and save CSVs
-def fetch_and_save_csv(uuid, category, name, riding, party, master_df):
+# Function to fetch and save the CSV
+def fetch_and_save_csv(uuid, category, name, year, quarter):
     csv_url = f'https://www.ourcommons.ca/ProactiveDisclosure/en/members/{category}/{year}/{quarter}/{uuid}/csv'
     csv_response = requests.get(csv_url)
+    csv_response.encoding = 'utf-8'  # Explicitly set encoding to UTF-8
 
     if csv_response.status_code == 200:
-        csv_response.encoding = 'utf-8'
-        csv_text = csv_response.text
-        csv_data = StringIO(csv_text)
-        csv_df = pd.read_csv(csv_data)
+        csv_text = csv_response.text.split('\n', 1)[1]  # Skip the first line
+        
+        # Create folders if they do not exist
+        folder_path = f"./{year}/{quarter}/{category}"
+        os.makedirs(folder_path, exist_ok=True)
 
-        # Append additional columns
-        csv_df['Name'] = name
-        csv_df['Party'] = party
-        csv_df['Riding'] = riding
-        csv_df['Year'] = year
-        csv_df['Quarter'] = quarter
+        # Sanitize the name and create the full file path
+        safe_name = name.replace(" ", "_").replace("/", "_").replace("\\", "_").replace(":", "_")
+        file_name = f"{safe_name}_{year}_{quarter}_{category}.csv"
+        file_path = os.path.join(folder_path, file_name)
 
-        # Append this DataFrame to the master DataFrame
-        master_df = pd.concat([master_df, csv_df], ignore_index=True)
-        return master_df
+        with open(file_path, "wb") as f:
+            f.write(csv_text.encode('utf-8'))
+
+        print(f"Successfully downloaded {category} CSV for {name} with UUID: {uuid} in {year}, {quarter}")
+
     else:
-        logs.append(f"Failed for {name} with URL: {csv_url}")
-        return master_df
+        print(f"Failed to download {category} CSV for {name} with UUID: {uuid} in {year}, {quarter}. URL: {csv_url}")
 
-# Iterate over the range of years and quarters
-for year in range(2021, 2024):
-    for quarter in range(1, 5):
-        # Skip Q1 2021 to start from Q2 2021
-        if year == 2021 and quarter == 1:
+# Start at Q2 2021
+start_year = 2021
+start_quarter = 2
+
+# End at Q4 2023
+end_year = 2023
+end_quarter = 4
+
+# Iterate through the years and quarters
+for year in range(start_year, end_year + 1):
+    for quarter in range(start_quarter if year == start_year else 1, end_quarter + 1 if year == end_year else 5):
+        # Replace the URL with the correct year and quarter
+        table_url = f'https://www.ourcommons.ca/ProactiveDisclosure/en/members/{year}/{quarter}'
+
+        response = requests.get(table_url)
+        if response.status_code != 200:
+            print(f"Failed to fetch data for {year}, {quarter}")
             continue
 
-        # Initialize an empty list to hold the rows and master DataFrames for each category
+        soup = BeautifulSoup(response.text, 'html.parser')
+        rows = soup.find_all('tr', {'class': 'expenses-main-info'})
+
         data = []
-        travel_master_df = pd.DataFrame()
-        hospitality_master_df = pd.DataFrame()
-        contract_master_df = pd.DataFrame()
-
-        # Define the URL for the table page
-        table_url = f'https://www.ourcommons.ca/ProactiveDisclosure/en/members/{year}/{quarter}'
+        columns = ['Name', 'Riding', 'Party', 'Salary', 'Travel', 'Hospitality', 'Contracts', 'UUID']
         
-        # Fetch and parse the main table
-        response = requests.get(table_url)
-        if response.status_code == 200:
-            soup = BeautifulSoup(response.text, 'html.parser')
-            rows = soup.find_all('tr', {'class': 'expenses-main-info'})
+        for row in rows:
+            row_data = [td.text.strip() for td in row.find_all('td')]
+            a_tag = row.find_all('td')[4].find('a', href=True)
+            
+            if a_tag:
+                href = a_tag['href']
+                uuid = href.split('/')[-1]
+                row_data.append(uuid)
+            else:
+                row_data.append('N/A')
+            
+            data.append(row_data)
 
-            # Collect the main table data
-            columns = ['Name', 'Riding', 'Party', 'Salary', 'Travel', 'Hospitality', 'Contracts', 'UUID']
-            for row in rows:
-                row_data = [td.text.strip() for td in row.find_all('td')]
-                a_tag = row.find_all('td')[4].find('a', href=True)
-                if a_tag:
-                    href = a_tag['href']
-                    uuid = href.split('/')[-1]
-                    row_data.append(uuid)
-                else:
-                    row_data.append('N/A')
-                data.append(row_data)
-
-            df = pd.DataFrame(data, columns=columns)
-
-            for _, row in df.iterrows():
-                uuid = row['UUID']
-                name = row['Name']
-                riding = row['Riding']
-                party = row['Party']
-
-                if uuid != 'N/A':
-                    travel_master_df = fetch_and_save_csv(uuid, 'travel', name, riding, party, travel_master_df)
-                    hospitality_master_df = fetch_and_save_csv(uuid, 'hospitality', name, riding, party, hospitality_master_df)
-                    contract_master_df = fetch_and_save_csv(uuid, 'contract', name, riding, party, contract_master_df)
-
-            # Save the master DataFrames to CSV
-            travel_master_df.to_csv(f"{year}_Q{quarter}_travel.csv", index=False, encoding='utf-8')
-            hospitality_master_df.to_csv(f"{year}_Q{quarter}_hospitality.csv", index=False, encoding='utf-8')
-            contract_master_df.to_csv(f"{year}_Q{quarter}_contract.csv", index=False, encoding='utf-8')
-
-            # Log the failures, if any
-            with open(f"{year}_Q{quarter}_log.txt", 'w') as log_file:
-                for log in logs:
-                    log_file.write(log + '\n')
-        else:
-            print(f"Failed to fetch the main table for {year} Q{quarter}")
+        df = pd.DataFrame(data, columns=columns)
+        
+        # Download the CSVs
+        for _, row in df.iterrows():
+            uuid = row['UUID']
+            name = row['Name']
+            
+            if uuid != 'N/A':
+                for category in ['travel', 'hospitality', 'contract']:
+                    fetch_and_save_csv(uuid, category, name, year, quarter)
